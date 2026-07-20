ef9655195653c1ccd43e19ccc08e98403597739e
cbonic <120558619+cbonic@users.noreply.github.com>
2026-07-20 16:09:30 +0900

    Task2_10追加

--- 2-9/Task2_9.java
+++ 2-9/Task2_9.java
@@ -58,27 +58,27 @@ public class Task2_9 {
     // 以下の変数「name」「pass」を使用して、①〜④の条件を満たす処理を記述して下さい。
     // nameとpassはログイン画面からの入力値だと想定してみましょう。
 
-    String name = "alice";
-    String pass = "alice123";
+    String name = USER_NAME;
+    String pass = USER_PASSWORD;
 
     // ① 「name」の値が「USER_NAME」と等しく、「 pass 」の値が「USER_PASSWORD」と等しい場合。
     // 定数を使用して「 ログイン成功です。 」と出力して下さい。
 
-    if (name == "alice" && pass == "alice123")
+    if (name == USER_NAME && pass == USER_PASSWORD)
     {
       System.out.println(CONST_MSG_SUCCESS);
     }
 
     // ② 「USER_NAME」の値のみ等しい場合。
     // 定数を使用して「 パスワードに誤りがあります。 」 と出力して下さい。
-    if (name == "alice" && pass != "alice123")
+    if (name == USER_NAME && pass != USER_PASSWORD)
     {
       System.out.println(CONST_MSG_ERROR_PASS);
     }
 
     // ③ 「USER_PASSWORD」の値のみ等しい場合。
     //定数を使用して「 名前に誤りがあります。 」と出力して下さい。
-    if (name != "alice" && pass == "alice123")
+    if (name != USER_NAME && pass == USER_PASSWORD)
     {
       System.out.println(CONST_MSG_ERROR_NAME);
     }
@@ -86,7 +86,7 @@ public class Task2_9 {
     // ④ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。
     //定数を使用して「 入力情報に誤りがあります。 」と出力して下さい。
 
-    if(name != "alice" && pass != "alice123")
+    if(name != USER_NAME && pass != USER_PASSWORD)
     {
       System.out.println(CONST_MSG_ERROR_INPUT);
     }
